c769e30c2d4573c9ea8c24da01a8842c362d31d0
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-09-14 19:09:54 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,7 +41,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
+	let gain = new Decimal(0)
 	return gain
 }
 
@@ -56,7 +56,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("11"))
+	return player.points.gte(new Decimal("1000000"))
 }
 
 
