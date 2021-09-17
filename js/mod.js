36eaea14bc30b35e622538a829377bedd504d9c5
CleanCutBloons <61286931+CleanCutBloons@users.noreply.github.com>
2021-09-17 18:11:16 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -60,7 +60,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("1000000"))
+	return player.points.gte(new Decimal("1e1000"))
 }
 
 
